228d985016b95b4b634607fe78d0a100ba9f6efd
Darian Ngo <darianngo@gmail.com>
2023-04-09 22:00:22 -0500

    Added MatchService

---
+++ src/main/java/com/darianngo/discordBot/entities/MatchEntity.java
@@ -0,0 +1,21 @@
+package com.darianngo.discordBot.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class MatchEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+}
