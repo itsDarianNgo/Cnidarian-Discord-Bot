37cb958c9fac252d7f5f46ec622e1ca6373bf2bf
Darian Ngo <darianngo@gmail.com>
2023-04-18 12:48:51 -0500

    Fixed vote timeout message always being set

--- src/main/java/com/darianngo/discordBot/listeners/ButtonClickListener.java
+++ src/main/java/com/darianngo/discordBot/listeners/ButtonClickListener.java
@@ -110,7 +110,7 @@ public class ButtonClickListener extends ListenerAdapter {
 			User user = event.getJDA().retrieveUserById(userDTO.getDiscordId()).complete();
 			votingService.sendVotingDM(user, matchId);
 		}
-		// Start 5-minute countdown
+		// Start 10-minute countdown
 		votingService.startVoteCountdown(event, matchId);
 
 		// Disable the "End Match" button
@@ -335,8 +335,8 @@ public class ButtonClickListener extends ListenerAdapter {
 				sendApprovalRequest(event, matchResult, matchId);
 				usersFullyVoted.remove(matchId);
 
-				// Cancel scheduled admin voting
-				executorService.shutdown();
+				// Cancel the scheduled admin voting
+				votingService.cancelVoteCountdown(matchId);
 
 			}
 		}
